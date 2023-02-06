ab429ff430f786951daae3bcf12f6826e0862f35
ashikstylist <41476160+ashikstylist@users.noreply.github.com>
2023-02-06 02:31:11 -0800

    Updated Owl Carousel

--- js/main.js
+++ js/main.js
@@ -1,4 +1,69 @@
 $(document).ready(function () {
+
+  // Exclusive Carousel
+  $(".owl-carousel-exclusive").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+        loop: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+        loop: false,
+      },
+      991: {
+        items: 3,
+        nav: false,
+        loop: false,
+      },
+    },
+  });
+  $('.exclusive-prev').click(function() {
+    $(".owl-carousel-exclusive").trigger('prev.owl.carousel');
+  })
+  $('.exclusive-next').click(function() {
+    $(".owl-carousel-exclusive").trigger('next.owl.carousel');
+  })
+
+
+  // Travel Section Carousel
+
+  $(".owl-carousel-travel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+        loop: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+        loop: false,
+      },
+      991: {
+        items: 4,
+        nav: false,
+        loop: false,
+      },
+    },
+  });
+  $('.travel-prev').click(function() {
+    $(".owl-carousel-travel").trigger('prev.owl.carousel');
+  })
+  $('.travel-next').click(function() {
+    $(".owl-carousel-travel").trigger('next.owl.carousel');
+  })
+
+
+
   // Toggle Dashboard Card
   $(".dash-card-toggle").click(function () {
     $(".dash-card-toggle").removeClass("dash-card-toggle-active");
